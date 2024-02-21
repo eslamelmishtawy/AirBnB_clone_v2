6b0fd25f17ce27bd0c58babed5c0a5f6c3cfc00e
root <omar.elmishtawy@gmail.com>
2024-02-21 10:55:45 -0800

    delete obj from file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls == None:
+            return FileStorage.__objects
+        cls_objs_dict = {}
+        for k, v in self.__objects.item():
+            if cls == v.__class__ or cls == v.__class__.__name__:
+                cls_objs_dict[k] = v
+        return cls_objs_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
